b9d1f9dcebc549869e5418c86f42a5e822d65246
lthon09 <>
2022-10-03 23:03:01 +0700

    latest changes

--- build/index.js
+++ build/index.js
@@ -1,16 +1,29 @@
 const { exit } = require("process");
 const fs = require("fs");
 
-const sourcePath = "../src/ui/css/src";
-const distributePath = "../src/ui/css/dist";
+const postcss = require("postcss");
 
-fs.readdir(sourcePath, (error, items) => {
+const sass = require("sass");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const terser = require("terser");
+
+const cssSourcePath = "../src/ui/css/src";
+const cssDistributePath = "../src/ui/css/dist";
+
+const jsSourcePath = "../src/ui/js/src";
+const jsDistributePath = "../src/ui/js/dist";
+
+const cssProcessor = postcss([autoprefixer(), cssnano()]);
+const jsMinifierOptions = JSON.parse(fs.readFileSync(".terserrc.config.json"));
+
+fs.readdir(cssSourcePath, (error, items) => {
     if (error) {
         exit(1);
     }
 
     items.forEach((item, _) => {
-        const path = `${sourcePath}/${item}`;
+        const path = `${cssSourcePath}/${item}`;
 
         fs.stat(path, async (error, statistics) => {
             if (error) {
@@ -19,8 +32,37 @@ fs.readdir(sourcePath, (error, items) => {
 
             if (statistics.isFile()) {
                 const compiled = (await sass.compileAsync(path)).css;
+                const processed = (await cssProcessor.process(compiled)).css;
 
-                fs.writeFile(`${distributePath}/${item.slice(0, -5)}.css`, compiled, (_) => {});
+                fs.writeFile(`${cssDistributePath}/${item.slice(0, -5)}.min.css`, processed, (_) => {});
+            }
+        });
+    });
+});
+
+fs.readdir(jsSourcePath, (error, items) => {
+    if (error) {
+        exit(1);
+    }
+
+    items.forEach((item, _) => {
+        const path = `${jsSourcePath}/${item}`;
+
+        fs.stat(path, async (error, statistics) => {
+            if (error) {
+                return;
+            }
+
+            if (statistics.isFile()) {
+                fs.readFile(path, async (error, data) => {
+                    if (error) {
+                        return;
+                    }
+
+                    const minified = (await terser.minify(data.toString(), jsMinifierOptions)).code;
+
+                    fs.writeFile(`${jsDistributePath}/${item.slice(0, -3)}.min.js`, minified, (_) => {});
+                });
             }
         });
     });
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
-fn main() {
+extern crate serde_json;
 
+use std::{
+    process,
+    error,
+    string,
+    path,
+    fs,
+};
+
+fn read_file(path : &str) -> Result<string::String, Box<dyn error::Error>> {
+    Ok(fs::read_to_string(path) ?)
+}
+
+fn parse_json(data : &str) -> Result<serde_json::Value, Box<dyn error::Error>> {
+    Ok(serde_json::from_str(data) ?)
+}
+
+fn parse_json_file(path : &str) -> Result<serde_json::Value, Box<dyn error::Error>> {
+    let content = read_file(path) ?;
+    let data = parse_json(content.as_str()) ?; // FIXME
+
+    Ok(data)
+}
+
+fn delete_old_version() {
+    let _path = path::Path::new("old_mozuli.exe");
+
+    if _path.exists() {
+        fs::remove_file(_path).ok();
+    }
+}
+
+fn check_updates() { // TODO
+}
+
+fn update() {} // TODO
+
+fn display_dialog(title : &str, message : &str) { // TODO
+
+}
+
+fn display_critical_error(error : &str) { // TODO
+    process::exit(1);
+}
+
+fn main() {
+    const API_VERSION : &str = "v1";
+
+    let development_mode = path::Path::new("../development").exists();
+
+    let application_data = match parse_json_file("resources/data/application.json") {
+        Ok(data) => data,
+        Err(error) => {
+            display_critical_error("Couldn't parse application data!");
+
+            error // FIXME
+        },
+    };
+
+    let server = ((match development_mode {
+        true => "http://localhost:5000",
+        false => "https://mozuli.deta.dev",
+    }).to_owned() + "/api/") + API_VERSION;
+
+    delete_old_version();
+    check_updates();
 }
