f04a8358e47f072d250ea534a414c69284999e4a
lthon09 <>
2022-10-06 21:04:07 +0700

    latest changes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 extern crate serde_json;
-extern crate reqwest;
+extern crate tauri;
 
 use std::{
     process,
     error,
     string,
     thread,
+    time::Duration,
     path,
     fs,
 };
+
+use tauri::{
+    api::{
+        http,
+    },
+};
+
+fn delete_old_version(old_executable : &str) {
+    let _path = path::Path::new(old_executable);
+
+    if _path.exists() {
+        fs::remove_file(_path).ok();
+    }
+}
+
+fn check_instances(application_data : &serde_json::Value, process_id : &String) {
+    if application_data["processId"] == process_id.to_string() {
+        process::exit(0);
+    }
+}
+
+fn write_instance(application_data : &serde_json::Value, process_id : &String) {
+    // TODO: edit "processId" in application_data
+}
 
 fn read_file(path : &str) -> Result<string::String, Box<dyn error::Error>> {
     Ok(fs::read_to_string(path) ?)
@@ -25,21 +50,30 @@ fn parse_json_file(path : &str) -> Result<serde_json::Value, Box<dyn error::Erro
     Ok(data)
 }
 
-fn delete_old_version() {
-    let _path = path::Path::new("old_mozuli.exe");
+async fn send_request(requests : &http::Client, method : &str, url : &str) -> Result<Result<http::Response, tauri::api::Error>, Box<dyn error::Error>> {
+    let request = http::HttpRequestBuilder::new(method, url) ?;
 
-    if _path.exists() {
-        fs::remove_file(_path).ok();
-    }
+    let response = requests.send(request).await;
+
+    Ok(response)
 }
 
-fn check_updates() {
+async fn check_updates(application_data : &serde_json::Value, requests : &http::Client, api_update : &String) -> Result<bool, Box<dyn error::Error>> {
+    let latest_version;
+
+    match send_request(requests, "get", &(api_update.to_owned() + "/latest_version")).await {
+        Ok(response) => latest_version = response,
+        Err(_) => (), // TODO: return error
+    }
+
+    Ok(false)
 }
 
 fn update() {
 }
 
 fn display_dialog(title : &str, message : &str) {
+    // title form: [title] - Mozuli
 }
 
 fn display_critical_error(message : &str) {
@@ -51,34 +85,86 @@ fn display_critical_error(message : &str) {
 fn main() {
     const API_VERSION : &str = "v1";
 
+    const OLD_EXECUTABLE : &str = "mozuli.exe.old";
+    const CURRENT_EXECUTABLE : &str = "mozuli.exe";
+
+    let process_id = (process::id()).to_string();
+
     let development_mode = path::Path::new("../development").exists();
 
-    let application_data = parse_json_file("resources/data/application.json");
-    let user_data = parse_json_file("resources/data/user.json");
+    let _application_data = parse_json_file("resources/data/application.json");
+    let _user_data = parse_json_file("resources/data/user.json");
 
-    match application_data {
+    let application_data;
+    let user_data;
+
+    match _application_data {
         Ok(_) => (),
-        Err(_) => display_critical_error("Couldn't parse application data."),
+        Err(_) => display_critical_error("Couldn't retrieve application data."),
     };
 
-    match user_data {
+    application_data = _application_data.unwrap();
+
+    match _user_data {
         Ok(_) => (),
-        Err(_) => display_critical_error("Couldn't parse user data."),
+        Err(_) => display_critical_error("Couldn't retrieve user data."),
     };
 
-    let server = ((match development_mode {
+    user_data = _user_data.unwrap();
+
+    let server = (match development_mode {
         true => "http://localhost:5000",
         false => "https://mozuli.deta.dev",
-    }).to_owned() + "/api/") + API_VERSION;
+    }).to_owned();
 
-    let http = reqwest::Client::builder().build();
+    let api = (&server).to_owned() + "/api/" + API_VERSION;
 
-    match http {
+    let api_update = api + "/update";
+
+    let _requests = http::ClientBuilder::new()
+        .connect_timeout(Duration::from_secs(10))
+        .max_redirections(0)
+
+        .build();
+    let requests;
+
+    match _requests {
         Ok(_) => (),
         Err(_) => display_critical_error("Couldn't build HTTP client."),
     }
 
-    delete_old_version();
+    check_instances(&application_data, &process_id);
+    write_instance(&application_data, &process_id);
 
-    // TODO spawn thread to check for updates every 30 seconds
+    requests = _requests.unwrap();
+
+    delete_old_version(OLD_EXECUTABLE);
+
+    let mut check_updates_thread_running = true;
+
+    // thread::spawn(move || async { // FIXME
+    //     let interval = Duration::from_secs(30);
+
+    //     loop {
+    //         if !check_updates_thread_running {
+    //             break;
+    //         }
+
+    //         match check_updates(&application_data, &requests, &api_update).await {
+    //             Ok(result) => {
+    //                 match result {
+    //                     false => (),
+    //                     true => break,
+    //                 }
+    //             },
+    //             Err(_) => (),
+    //         }
+
+    //         thread::sleep(interval);
+    //     }
+
+    //     update();
+    // });
+
+    check_updates_thread_running = false;
 }
